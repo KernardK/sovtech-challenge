e0274225a8a4ebda422369fa8612fabd69e3b07b
KernardK <kernardk@gmail.com>
2021-11-25 13:38:47 +0200

    Created a simple 404 page

---
+++ pages/404.js
@@ -0,0 +1,19 @@
+import { BiErrorAlt } from "react-icons/bi";
+
+import Link from "next/link";
+import Layout from "../components/Layout";
+import styles from "../styles/404.module.css";
+
+export default function NotFoundPage() {
+  return (
+    <Layout title="Page Not Found">
+      <div className={styles.error}>
+        <h1>
+          <BiErrorAlt /> 404
+        </h1>
+        <h4>Appologies, There is nothing to see here</h4>
+        <Link href="/">Let's Go Home</Link>
+      </div>
+    </Layout>
+  );
+}
